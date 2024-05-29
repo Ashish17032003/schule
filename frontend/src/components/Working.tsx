ceae47b5aa40183cf8410c3ee61d82960563653f
Ashish17032003 <ashishkv02@gmail.com>
2024-05-29 17:17:05 +0530

    embed new

--- frontend/src/components/Working.tsx
+++ frontend/src/components/Working.tsx
@@ -1,18 +1,23 @@
 const Working = () => {
-    return (
-        <>
-            <div className="flex flex-col justify-center items-center gap-10 m-20">
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center gap-10 m-20">
+        <p className="text-3xl font-mono font-extrabold">
+          How FormsDunia works
+        </p>
+        <iframe
+          width="720"
+          height="370"
+          src="https://www.youtube.com/embed/a5uQMwRMHcs?si=K55uDwddbeVrV3CK"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </>
+  );
+};
 
-                <p className="text-3xl font-mono font-extrabold">How FormsDunia works</p>
-                <iframe
-                    className="w-1/2 h-72"
-                    src='https://www.youtube.com/embed/E7wJTI-1dvQ'
-                    allow='autoplay; encrypted-media'
-                    title='video'
-                />
-            </div>
-        </>
-    )
-}
-
-export default Working
+export default Working;
