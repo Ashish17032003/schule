424de171baf1de5d15a36f9c633157e2e36bb97a
Ashish17032003 <ashishkv02@gmail.com>
2024-05-29 16:43:47 +0530

    testimonial

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -2,6 +2,7 @@ import Body from "../components/Body";
 import Facts from "../components/Facts";
 import Features from "../components/Features";
 import NavbarLanding from "../components/NavbarLanding";
+import Testimonial from "../components/Testimonial";
 import Working from "../components/Working";
 
 const Landing = () => {
@@ -12,6 +13,7 @@ const Landing = () => {
       <Facts />
       <Features />
       <Working />
+      <Testimonial />
     </>
   );
 };
